55fd04242f10d212f9bdceff8a172a3421b3cc3b
Sergiy-lv <caponchic6@gmail.com>
2021-02-22 17:05:51 +0200

    Sample code

--- js/index.js
+++ js/index.js
@@ -1,30 +1,32 @@
-const images = [
+class Carousel {
+    constructor(images) {
+        this.images = images;
+        this.currIdx = 0;
+        this.showCurrentImage();
+        document.querySelector('.prev').addEventListener('click',
+            () => this.prevImage());
+        document.querySelector('.next').addEventListener('click',
+            () => this.nextImage());
+        setInterval(() => this.nextImage(), 3000);
+    }
+    showCurrentImage() {
+        document.querySelector('.current').src = this.images[this.currIdx];
+    }
+    nextImage() {
+
+        if (++this.currIdx >= this.images.length) this.currIdx = i0;
+        this.showCurrentImage();
+    }
+    prevImage() {
+        if (--this.currIdx < 0) this.currIdx = this.images.length - 1;
+        this.showCurrentImage();
+    }
+}
+
+new Carousel([
     'img/s5.jpg',
     'img/m4-2.JPG',
     'img/q7.jpg',
     'img/340i.JPG',
-    'img/instagram.jpg',
     'img/A6.jpg'
-]
-let currIdx = 0;
-
-function showCurrentImage() {
-    const imgContainer = document.querySelector('.current');
-    imgContainer.src = images[currIdx];
-}
-showCurrentImage();
-
-function nextImage() {
-    currIdx++;
-    if (currIdx >= images.length) currIdx = 0;
-    showCurrentImage();
-
-}
-
-function prevImage() {
-    currIdx--;
-    if (currIdx < 0) currIdx = images.length - 1;
-    showCurrentImage();
-}
-document.querySelector(''.prev).addEventListener('click', prevImage);
-document.querySelector(''.next).addEventListener('click', prevImage);
+]);
